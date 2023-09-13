2fcf8e1f08ddcf069d14c693a081af9f3c8c1c26
toras9000 <toras9000@example.com>
2023-09-13 19:35:58 +0900

    プロジェクト作成

---
+++ DotNetUriPart/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace DotNetUriPart.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ DotNetUriPart/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DotNetUriPart.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
